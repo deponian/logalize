d29ee18b24e5f31f3a17c078da92acb693bafbc8
Rufus Deponian <rufus@deponian.com>
2025-10-24 17:53:02 +0500

    refactor: replace io.StringWriter with io.Writer in core.Run()

--- internal/core/core.go
+++ internal/core/core.go
@@ -12,7 +12,7 @@ import (
 
 // Run reads lines from the reader, colorizes them based on the settings
 // and writes the lines to the writer
-func Run(reader io.Reader, writer io.StringWriter, settings config.Settings) error {
+func Run(reader io.Reader, writer io.Writer, settings config.Settings) error {
 	hl, err := highlighter.NewHighlighter(settings)
 	if err != nil {
 		return err
@@ -30,14 +30,14 @@ func Run(reader io.Reader, writer io.StringWriter, settings config.Settings) err
 		if b == '\r' || b == '\n' || readErr == io.EOF {
 			// b is equal to 0 when we reach the end of the file
 			// so we won't add it to the end of the string
-			lastCharacter := ""
+			var lastCharacter string
 			if b == '\r' || b == '\n' {
 				lastCharacter = string(b)
 			}
 
 			colored := hl.Colorize(buffer.String())
 
-			_, err := writer.WriteString(colored + lastCharacter)
+			_, err := writer.Write([]byte(colored + lastCharacter))
 			if err != nil {
 				return err
 			}
